8e2a9be151e83d0822804d90172797d54022c36f
toyobayashi <lifenglin314@outlook.com>
2019-08-28 11:20:28 +0800

    path

--- util/path.js
+++ util/path.js
@@ -1,8 +1,16 @@
-const { join, resolve } = require('path')
+const { join, resolve, isAbsolute } = require('path')
 
 const context = process.env.TY_CONTEXT ? resolve(process.env.TY_CONTEXT) : process.cwd()
 
 function getPath (...relative) {
+  if (!relative.length) {
+    return context
+  }
+
+  if (isAbsolute(relative[0])) {
+    return join(...relative)
+  }
+
   return join(context, ...relative)
 }
 
