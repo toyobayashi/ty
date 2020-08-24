2cb121d4944c1df85bccb6d97442ce962a0a297a
toyobayashi <lifenglin314@outlook.com>
2020-08-24 17:15:03 +0800

    extensions

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -690,7 +690,7 @@ class WebpackConfig {
       externals: [webpackNodeExternals(config.nodeExternals)],
       resolve: {
         alias: config.alias,
-        extensions: ['.js', '.ts', '.json', '.node']
+        extensions: ['.tsx', '.ts', '.mjs', '.js', '.json', '.node', '.wasm']
       },
       plugins: [
         this._createDefinePlugin(config),
@@ -722,7 +722,7 @@ class WebpackConfig {
       },
       resolve: {
         alias: config.alias,
-        extensions: ['.ts', '.tsx', '.js', ...(this._useBabel ? ['.jsx'] : []), '.vue', '.css', '.styl', '.stylus', '.less', '.sass', '.scss', '.json', '.wasm']
+        extensions: ['.tsx', '.ts', '.mjs', '.js', ...(this._useBabel ? ['.jsx'] : []), '.vue', '.styl', '.stylus', '.less', '.sass', '.scss', '.css', '.json', '.wasm']
       },
       plugins: [
         ...(this._createHtmlPlugins(config)),
@@ -760,7 +760,7 @@ class WebpackConfig {
       externals: [webpackNodeExternals(config.nodeExternals)],
       resolve: {
         alias: config.alias,
-        extensions: ['.js', '.ts', '.json', '.node']
+        extensions: ['.tsx', '.ts', '.mjs', '.js', '.json', '.node', '.wasm']
       },
       plugins: [
         new CopyWebpackPlugin({
@@ -808,7 +808,7 @@ class WebpackConfig {
       },
       resolve: {
         alias: config.alias,
-        extensions: ['.ts', '.tsx', '.js', ...(this._useBabel ? ['.jsx'] : []), '.vue', '.css', '.styl', '.stylus', '.less', '.sass', '.scss', '.json', '.wasm']
+        extensions: ['.tsx', '.ts', '.mjs', '.js', ...(this._useBabel ? ['.jsx'] : []), '.vue', '.styl', '.stylus', '.less', '.sass', '.scss', '.css', '.json', '.wasm']
       },
       plugins: [
         ...(this._createHtmlPlugins(config)),
@@ -854,7 +854,7 @@ class WebpackConfig {
       },
       resolve: {
         alias: config.alias,
-        extensions: ['.ts', '.tsx', '.js', ...(this._useBabel ? ['.jsx'] : []), '.vue', '.css', '.styl', '.stylus', '.less', '.sass', '.scss', '.json', '.wasm']
+        extensions: ['.tsx', '.ts', '.mjs', '.js', ...(this._useBabel ? ['.jsx'] : []), '.vue', '.styl', '.stylus', '.less', '.sass', '.scss', '.css', '.json', '.wasm']
       },
       plugins: [
         this._createDefinePlugin(config),
